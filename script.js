55edc9dae47af761797d6f3c90f314298558cd0e
Rhiannon <154023480+RSpencerosa@users.noreply.github.com>
2025-08-13 20:29:42 -0700

    Add functionality

--- script.js
+++ script.js
@@ -15,17 +15,68 @@ function divide(a,b) {
 }
 
 function operate(fir,op,sec) {
-    if (op == "add") {
-        add(fir,sec)
-    } else if (op == "subtract") {
-        subtract(fir,sec)
-    } else if (op == "multiply") {
-        multiply(fir,sec)
-    } else if (op == "divide") {
-        divide(fir,sec)
+    if (op == "+") {
+        return add(fir,sec)
+    } else if (op == "-") {
+        return subtract(fir,sec)
+    } else if (op == "*") {
+        return multiply(fir,sec)
+    } else if (op == "/") {
+        return divide(fir,sec)
     }
 }
+
+function clear() {
+    fir = undefined
+    op = undefined
+    sec = undefined
+}
+
+const display = document.querySelector(".display")
+const numButtons = document.querySelectorAll(".number")
+const opButtons = document.querySelectorAll(".operator")
+const equals = document.querySelector("#operate")
 
 let fir
 let op
 let sec
+
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent = button.textContent
+        if (fir == undefined) {
+            fir = Number(button.textContent)
+        } else if (fir && op == undefined) {
+            display.textContent = "error"
+            clear()
+        } else if (fir && op) {
+            sec = Number(button.textContent)
+        }
+        console.log(fir,op,sec)
+    })
+})
+
+opButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent = button.textContent
+        if (op == undefined) {
+            op = button.textContent
+        } else {
+            display.textContent = "error"
+            clear()
+        }
+        console.log(fir,op,sec)
+    })
+})
+
+equals.addEventListener("click", () => {
+    if (fir && op && sec) {
+        let result = operate(fir,op,sec)
+        display.textContent = result
+        console.log(result)
+        clear()
+    } else {
+        display.textContent = "error"
+        clear()    
+    }
+})
